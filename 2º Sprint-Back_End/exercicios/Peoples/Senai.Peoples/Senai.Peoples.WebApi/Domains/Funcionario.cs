70a6ddd0fc2d9a37e3c4e0231ed1bdce976fc76f
rafaelgckto <rafael.gcecatto@gmail.com>
2021-03-26 14:07:47 -0300

    Deletar diretório 2º Sprint

--- 2º Sprint-Back_End/exercicios/Peoples/Senai.Peoples/Senai.Peoples.WebApi/Domains/Funcionario.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Senai.Peoples.WebApi.Domains {
-    public class Funcionario {
-        public int idFuncionario { get; set; }
-        public string nome { get; set; }
-        public string sobrenome { get; set; }
-    }
-}
