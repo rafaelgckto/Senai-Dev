55852fd9f1387332ec5a4d5b6fe4d8360df93dca
rafaelgckto <rafael.gcecatto@gmail.com>
2021-04-12 11:45:18 -0300

    Adição de query de consulta e endpoint, NomeCompleto e Ordenação

--- 2º Sprint-Back_End/exercicios/Peoples/Modeling Scripts/people_DQL.sql
+++ 2º Sprint-Back_End/exercicios/Peoples/Modeling Scripts/people_DQL.sql
@@ -1,3 +1,14 @@
 USE Peoples;
 
 SELECT * FROM Funcionario;
+
+SELECT nome, sobrenome
+FROM Funcionario
+WHERE nome = 'Catarina';
+
+SELECT CONCAT(nome, ' ', sobrenome) AS [Nome Completo]
+FROM Funcionario;
+
+SELECT nome, sobrenome
+FROM Funcionario
+ORDER BY nome ASC;--ASC OU DESC
